01590b8f4c8277a76d2a2457becb055f1c8f93ff
Nguyen Van Quyen <153894626+quyen0505@users.noreply.github.com>
2023-12-15 15:16:03 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,25 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Thong Tin sv
+		 */
+		String name = " Nguyen Van Quyen ";
+		int msv = 123456;
+		double cmnd = 123456789;
+		String tenLop = "23CDTH41";
+		String tenTruong = "CDCN HUE";
+		double dtb = 9f;
+
+		System.out.println(" Ho va ten : " + name);
+		System.out.println(" Ma sinh vien : " + msv);
+		System.out.println(" Ten lop : " + tenLop);
+		System.out.println(" Ten Truong : " + tenTruong);
+		System.out.println(" Diem trung binh : " + dtb);
+
+	}
+
+}
